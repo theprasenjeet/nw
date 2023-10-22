862eb8e01dcd36bb9208a2b595fa17ce03bf65b9
PRASEN JEET <theprasenjeet@gmail.com>
2023-10-22 11:20:45 +0530

    Update App_Streamlit.py

--- App_Streamlit.py
+++ App_Streamlit.py
@@ -2,11 +2,10 @@
 import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split  # Add this line to import train_test_split
+from sklearn.model_selection import train_test_split  
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load your dataset (replace with your dataset path)
 data = pd.read_csv("Water.csv")  # Replace with your dataset path
 
 # Removing outliers using the IQR method
@@ -15,12 +14,9 @@ Q3 = data['Water_Loss_Percentage'].quantile(0.75)
 IQR = Q3 - Q1
 lower_bound = Q1 - 1.5 * IQR
 upper_bound = Q3 + 1.5 * IQR
-
 data = data[(data['Water_Loss_Percentage'] >= lower_bound) & (data['Water_Loss_Percentage'] <= upper_bound)]
 
 
-
-# Create a Streamlit app
 st.title("Visualization of Relationships")
 
 # User input for selecting columns to visualize
@@ -36,7 +32,6 @@ else:
 
 
 
-
 # Split the data into training and testing sets
 X = data[['Pipe Diameter_inches', 'Distance_miles']]
 y = data['Water_Loss_Percentage']
@@ -44,7 +39,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Create the Random Forest regression model
 model = RandomForestRegressor(n_estimators=100, random_state=42)
-
 # Train the model on the training data
 model.fit(X_train, y_train)
 
@@ -55,7 +49,8 @@ def predict_water_loss(pipe_diameter, distance_miles):
     prediction = model.predict(features)
     return prediction[0]
 
-# Create a Streamlit app
+
+
 st.title("NRW Prediction App")
 
 
