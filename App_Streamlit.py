9eb82d1fcb0c5aad074eca3c7d38df90b09276b2
PRASEN JEET <theprasenjeet@gmail.com>
2023-10-22 13:50:59 +0530

    Update App_Streamlit.py

--- App_Streamlit.py
+++ App_Streamlit.py
@@ -89,4 +89,10 @@ st.markdown(
     </style>
     """,
     unsafe_allow_html=True,)
-
+hide_streamlit_style = """
+            <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
