95f5865cc217443e63e8e67acedc2bd59e46c055
PRASEN JEET <theprasenjeet@gmail.com>
2023-10-22 11:03:22 +0530

    Update App_Streamlit.py

--- App_Streamlit.py
+++ App_Streamlit.py
@@ -2,6 +2,9 @@
 import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 
